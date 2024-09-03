3ac61c84e5eab06d09ed8887140e46267b947dbd
Animized <112134688+Animized@users.noreply.github.com>
2024-09-03 09:32:21 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,6 +98,7 @@ export default {
                             </li>
                         </ol>
                     </template>
+                    <h3>Level Requirements</h3>
                     <h3>Submission Requirements</h3>
                     <p>
                         Achieved the record without using hacks
